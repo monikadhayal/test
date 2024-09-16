b7d605fa845c5aa170022c02f82eb8e242449c89
monikadhayal <monikadhayal25@gmail.com>
2024-09-16 20:57:51 +0530

    1-test

---
+++ nonu/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1445553
+let accountEmail = "monika@google.com"
+var accountPassword = "1234"
+accountCity ="jaipur"
+let accountState:
+
+// accountId = 90
+
+accountEmail="mona@google.com"
+accountPassword="123"
+accountCity ="sikar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
