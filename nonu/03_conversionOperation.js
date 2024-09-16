67a1da2296203b52911fb9f377710514ec70d20d
monikadhayal <monikadhayal25@gmail.com>
2024-09-17 02:43:33 +0530

    some change in 3 test

--- nonu/03_conversionOperation.js
+++ nonu/03_conversionOperation.js
@@ -13,11 +13,42 @@ let valueInNumber = Number(score)
 let isLoggedIn =0;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 //1 = true; 0= false; 
 //"" = false ; "monika" = true
 
 let monika = 33
 
 let akshay = String(monika)
-console.log( typeof akshay) 
+//console.log( typeof akshay) 
+
+//************************* Operation ************************//
+ let value =3
+ let negvalue = -value
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 ="monika"
+let str3 = str1+str2
+console.log(str3)
+console.log(str1+str2)
+
+let num, num2, num3 
+ num1 = num2 =num3 = 2+2
+ console.log(num1,num2,num3)
+
+ let gameCounter =100
+ gameCounter++;//postfixed
+ ++gameCounter; //prefixed
+ console.log(gameCounter);
+
+
+
+
+ 
