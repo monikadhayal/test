a802a3462ba6995e678788349859b632f8dcd9dd
monikadhayal <monikadhayal25@gmail.com>
2024-09-16 23:55:50 +0530

    2-test

---
+++ nonu/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version 
+//alert(3+3)  we are using nodejs, not browser
+
+console.log(3+3)
+
+ console.log("monika")
+
+ let name ="monika"
+ let age = 18
+ let isLoggedIN = false
+
+console.log(typeof age);
+
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined
+
