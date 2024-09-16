7bfaea875048436a3fe1cc55fe1363853fb4f480
monikadhayal <monikadhayal25@gmail.com>
2024-09-17 05:01:16 +0530

    test 5(datatype summary)

---
+++ nonu/datatype_summary.js
@@ -0,0 +1,40 @@
+ //primitive
+
+ // 7 type : String ,number , Boolearn ,null, undefined, Symbol, bigInt
+
+ 
+
+ //javascript dynamic langvage h
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIN = false 
+ const outsideTemp = null
+ 
+ let username;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id === anotherId)
+
+ const bigNumber = 1234567812345671234567n
+
+//reference (non primitive)
+ // array, object , function 
+
+const heros = ["shaktiman" ,"naagraj" ,"doga"]
+let myObj={
+   
+    name:"monika",
+    age:"23",
+}
+
+const myfunction = function(){
+    console.log("hello world!")
+}
+
+console.log( typeof bigNumber)
+
+console.log(typeof myfunction);
