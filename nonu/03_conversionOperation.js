199abb661d4ac1788ca23f42ca7bf83ae16a3583
monikadhayal <monikadhayal25@gmail.com>
2024-09-17 02:02:37 +0530

    3-test

---
+++ nonu/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "monika"
+
+
+//console.log(typeof score);
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+let isLoggedIn =0;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1 = true; 0= false; 
+//"" = false ; "monika" = true
+
+let monika = 33
+
+let akshay = String(monika)
+console.log( typeof akshay) 
