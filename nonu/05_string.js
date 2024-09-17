d7e917be6bc85e36aa33a66f08f7b858a94c5a0c
monikadhayal <monikadhayal25@gmail.com>
2024-09-17 07:59:56 +0530

    test-6

---
+++ nonu/05_string.js
@@ -0,0 +1,32 @@
+const name ="monika"
+const repoCount = 50
+// console.log(name + repoCount +"value")
+
+console .log(`hello my name is ${name} and my repo count is ${ repoCount }`);
+
+const gameName = new String("monika-chahar")
+
+// console.log(gameName)
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+ const newString = gameName.substring(0,4)
+//  console.log(newString);
+
+ const anotherString = gameName.slice(-6, 4);
+ console.log(anotherString);
+
+ const newStringOne =" minyka  "
+ console.log(newStringOne)
+ console.log(newStringOne.trim());
+
+ const url ="minlarere fv  vjfjrfffn fvdf"
+ console.log(url)
+ console.log(url.replace('%20','-'))
