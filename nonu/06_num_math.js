c985076068a0d63c14dc4ccb83663cac0a992060
monikadhayal <monikadhayal25@gmail.com>
2024-09-25 08:30:01 +0530

    somechang_test -6

--- nonu/06_num_math.js
+++ nonu/06_num_math.js
@@ -1,1 +1,31 @@
 const score =400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(6))
+
+// const otherNumber = 123.1934
+
+// console.log(otherNumber.toPrecision(4))
+
+const  hundreds =100000000
+//console.log(hundreds.toLocalString('en-IN'))
+
+//******************math**************** *//
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3,4,5,6))
+// console.log(Math.max(4,5,6,7))
+
+ console.log(Math.random()); 
+ console.log(Math.random()*10 + 1);
+ 
