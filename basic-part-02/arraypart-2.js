dca98a7c1774d256ca8ae0e5602f0f16e5d4f151
monikadhayal <monikadhayal25@gmail.com>
2024-09-26 01:53:52 +0530

    arraypart-002

---
+++ basic-part-02/arraypart-2.js
@@ -0,0 +1,29 @@
+const marvel_hero =["thod" , "ironman" , "monika"]
+const dc_hero =["1" ,"akshay" , "c5"]
+
+//marvel_hero.push(dc_hero)
+// console.log(marvel_hero)
+
+// console.log(marvel_hero[3][1])
+
+//const allHero = marvel_hero.concat(dc_hero)
+//console.log(allHero)
+
+// const all_new_hero = [...marvel_hero, ...dc_hero]
+// console.log(all_new_hero)
+
+// const another_array = [1,2,3,[4,5,6] ,7,[6,7,[3,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array)
+
+
+// console.log(Array.isArray("monika"))
+// console.log(Array.from("monika"))
+// console.log(Array.from({name:"monika"}))//intersting
+
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1, score2, score3))
