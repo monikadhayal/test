ed3a23e16d3f07bf39c94e978744cb28a78fbf70
monikadhayal <monikadhayal25@gmail.com>
2024-09-29 02:45:31 +0530

    test-13

--- basic-part-02/giobal-local-scope.js
+++ basic-part-02/giobal-local-scope.js
@@ -3,10 +3,44 @@ let a= 300
 if(true){
    let a = 10;
    const b = 20;
-   console.log("innner:",a)
+  // console.log("innner:",a)
 }
 
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+function one(){
+const username ="monika"
+
+    function two(){
+        const website ="youtube"
+        console.log(username)
+    }
+   // console.log(website)
+    two()
+}
+//one()
+if (true){
+    const username ="monika"
+    if(username === "monika"){
+        const website ="  youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//  console.log(username);
+
+//************interesting********************//
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(5))
