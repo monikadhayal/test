ef8def46a704790c8864e4296b9af0d2bb90e2b3
monikadhayal <monikadhayal25@gmail.com>
2024-09-28 18:39:40 +0530

    test_08

---
+++ basic-part-02/object-03.js
@@ -0,0 +1,16 @@
+const course ={
+    coursename: "js in hinidi",
+    price: "999",
+    courseInstructor: "akshay"
+}
+
+const { courseInstructor: Instructor} = course;
+
+console.log(Instructor)
+ //rendomuser me api tok
+ // jsn
+{
+    "name": "monika",
+   "coursename": "js in hindi",
+   "prize": "free"
+}
