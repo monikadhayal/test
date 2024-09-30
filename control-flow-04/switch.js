d0e62dc9945651d8acc17b7a0fd2d6dd65c90382
monikadhayal <monikadhayal25@gmail.com>
2024-09-30 09:59:05 +0530

    test 17

---
+++ control-flow-04/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value;
+//     break;
+
+//     default;
+//     break;
+// }
+
+const month = 2
+
+switch (month){
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("fabuary")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    default:
+        console.log("may")
+        break;            
+}
+
+//jab hum case2 par break nhi lagate h to aaye vale case chal jate h defoult nhi chalta h 
