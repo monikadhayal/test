fb9414e5eb5f81a40e16eef112c9970936318aff
monikadhayal <monikadhayal25@gmail.com>
2024-09-30 22:47:19 +0530

    while loop introduction

---
+++ loops-05/while-and-dowhile-loop.js
@@ -0,0 +1,24 @@
+
+let i =0 
+while(i <= 10){
+     //console.log(`value of i is ${i}`)
+     i =i+2
+}
+
+let myArray = ["m", "o","n","i","k","a"]
+
+let arr =0
+while(arr <myArray.length){
+  //console.log(`value of arr is ${myArray[arr]}`)
+  arr = arr + 1
+}
+
+//***************DO while ********** */
+ let score =1
+do{
+    console.log(`score is ${score}`);
+    score=score+2;
+}while("");
+
+// pata h " ",1 to infiniti loop chalta h or 0,"" is se 1 time only
+
