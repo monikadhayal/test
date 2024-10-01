e592b01cd15bc7a43bbb2e66aee5eee7bad25fbe
monikadhayal <monikadhayal25@gmail.com>
2024-10-02 04:05:37 +0530

    test 19

---
+++ loops-05/high-Order-Arrayloops.js
@@ -0,0 +1,54 @@
+
+
+const arr =[1,2,3,4,5]
+
+for(const i of arr) {
+    //console.log(i);
+
+}
+
+const monika = "hello world!"
+ for(const i of monika) {
+
+   // console.log(i);
+ }
+
+ //Maps
+
+ const  map = new Map() // map alwayes unice value store karta h or jis orede h me us me hi print karta h 
+
+ map .set('m',"monika")
+ map .set('m',"monika")
+ map .set('a',"akshay")
+ map .set('c',"chahar")
+
+ //console.log(map)
+
+ for(const [key,value] of map){
+   // console.log(key, ":-", value);
+ }
+
+ const myObject = {
+    game1 : 'ludo',
+    game2 : 'ludo2'
+ }
+
+ for(const [key , value] of myObject){
+   // console.log(key, ':-', value);
+ }
+ 
+
+
+
+
+
+
+
+
+// for(const[key,value] of map){
+//     console.log(key,':-',value);
+// }
+
+// const myObject ={
+//     'game1'
+// }
