c4185684c9b220ef1c89e36dbdb4e4970c9fa26e
monikadhayal <monikadhayal25@gmail.com>
2024-10-09 16:15:32 +0530

    test 23

--- jav.js
+++
@@ -1,2 +0,0 @@
-console.log ("monika")
-console.log("monika")
---
+++ script.js
@@ -0,0 +1,6 @@
+// console.dir(window.document.body);
+// // console.log(document.head);
+// // document.body.childNodes[3].innerText = "abcd";
+// //let heading = document.getElementById("heading");
+let heading = document.getElementById("heading")
+console.log(heading);
