bc5d0f1f39f47cd781749c38f268fe92f09481b7
monikadhayal <monikadhayal25@gmail.com>
2024-10-16 00:25:17 +0530

    BMI

--- project-1/script.js
+++
@@ -1,6 +0,0 @@
-// console.dir(window.document.body);
-// // console.log(document.head);
-// // document.body.childNodes[3].innerText = "abcd";
-// //let heading = document.getElementById("heading");
-let heading = document.getElementById("heading")
-console.log(heading);
