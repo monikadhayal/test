a0605709067fd22981042861fdb021f541473b2b
monikadhayal <monikadhayal25@gmail.com>
2024-10-18 02:50:07 +0530

    bmi

--- project-1/javascript-2.js
+++ project-1/javascript-2.js
@@ -1,6 +1,4 @@
 const form = document.querySelector("form");
-// this usecase will give you empty
-// const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -10,12 +8,43 @@ form.addEventListener("submit", function (e) {
   const results = document.querySelector("#results");
 
   if (height === "" || height < 0 || isNaN(height)) {
-    results.innerHTML = `Please give a valid height ${height}`;
+    results, (innerHTML = `please give a valid height ${height}`);
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
-    results.innerHTML = `Please give a valid weight ${weight}`;
+    results, (innerHTML = `please give a valid height ${weight}`);
   } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+   const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //    show the result
+    results.innerHTML = `<span>${bmi} </span>`;
   }
 });
+
+// function calculateBMI() {
+//   const weight = parseFloat(document.getElementById("weight").value);
+//   const height = parseFloat(document.getElementById("height").value) / 100; // Convert height from cm to meters
+
+//   if (!weight || !height || weight <= 0 || height <= 0) {
+//     document.getElementById("bmi-value").innerText = "--";
+//     document.getElementById("bmi-category").innerText =
+//       "Please enter valid values.";
+//     return;
+//   }
+
+//   const bmi = (weight / (height * height)).toFixed(2); // Calculate BMI
+//   document.getElementById("bmi-value").innerText = bmi;
+
+//   let category = "";
+
+//   if (bmi < 18.5) {
+//     category = "Underweight";
+//   } else if (bmi >= 18.5 && bmi < 24.9) {
+//     category = "Normal weight";
+//   } else if (bmi >= 25 && bmi < 29.9) {
+//     category = "Overweight";
+//   } else {
+//     category = "Obesity";
+//   }
+
+//   document.getElementById(
+//     "bmi-category"
+//   ).innerText = `You are in the ${category} category.`;
+// }
